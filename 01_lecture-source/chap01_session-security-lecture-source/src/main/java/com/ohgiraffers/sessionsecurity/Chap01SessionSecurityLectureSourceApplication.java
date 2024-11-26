f12300f8e37068efb602ad53b37600b8df4421bb
DESKTOP-ET10H8L\hi <dmssl2486@gmail.com>
2024-11-26 09:57:59 +0900

    [241126]

---
+++ 01_lecture-source/chap01_session-security-lecture-source/src/main/java/com/ohgiraffers/sessionsecurity/Chap01SessionSecurityLectureSourceApplication.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.sessionsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chap01SessionSecurityLectureSourceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chap01SessionSecurityLectureSourceApplication.class, args);
+    }
+
+}
